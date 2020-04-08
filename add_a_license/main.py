b05d97f5fa9414a739a3e60fd6985eca41379ac5
Bern Carney <18447002+BernCarney@users.noreply.github.com>
2020-04-08 13:09:57 -0600

    linting/formatting setup for project & editor

---
+++ add_a_license/main.py
@@ -0,0 +1,7 @@
+# Add A License
+#
+# Description: The goal of this CLI application is for a user to be able to easily add
+# various licenses to their project without having to cut and paste text.
+#
+# Workflow
+# 1.
--- tests/test_add_a_license.py
+++ tests/test_add_a_license.py
@@ -2,4 +2,4 @@ from add_a_license import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
