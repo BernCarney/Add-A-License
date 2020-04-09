dc08a6f9b0fce2b43035c16932f7ed7046af640e
Bern Carney <18447002+BernCarney@users.noreply.github.com>
2020-04-09 02:24:36 -0600

    added version function and callback option... took way to long

--- add_a_license/main.py
+++ add_a_license/main.py
@@ -8,8 +8,8 @@
 # 2. aalicense GET <LICENSE> with no options should get the license and put it in the
 #    current folder
 # 3. aalicense GET <LICENSE> [DIRECTORY] should place the license in the given path
-# 4. aalicense GET with no other input should give and interactive prompt to choose
-#    either A. from a list of top licenses OR B. A list of all licenses supported
+# 4. aalicense LIST should give and interactive prompt to choose either
+#    A. from a list of top licenses OR B. A list of all licenses supported
 # 5. After retrieving any license the cli should prompt for your <FULL NAME> to put in
 #    the license
 # 6. Ask if this a new project or not
@@ -18,14 +18,58 @@
 # 9. If (NO) but input year is same is current year, notify that you will not use range
 #    and just use current year
 
+from pathlib import Path
+
 import typer
+
+from add_a_license import __version__
 
 app = typer.Typer()
 
 
-@app.command()
-def test123():
+def print_version():
+    typer.echo(f"Add A License version: {__version__}")
+    raise typer.Exit()
+
+
+@app.callback()
+def main(
+    version: bool = typer.Option(
+        False, "--version", "-v", help="Print version and exit.", callback=print_version
+    )
+):
     """
-    Test the CLI application
+    A program to help you easily add a license to your project.
     """
-    typer.echo("Testing that it works?")
+
+
+@app.command("list")
+def list_licenses():
+    """
+    Prompt user for what license they would like to add
+    """
+    pass
+
+
+@app.command("get")
+def get_license(
+    license: str,
+    dir: Path = typer.Option(
+        ...,
+        "--dir",
+        "-d",
+        exists=True,
+        dir_okay=True,
+        file_okay=False,
+        writable=True,
+        resolve_path=True,
+    ),
+):
+    """
+    Get a LICENSE file and copy it to the project
+    """
+    pass
+
+
+if __name__ == "__main__":
+    app()
