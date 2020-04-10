9bacbb6d072e139625832c662e0a0c131c599e1f
Bern Carney <18447002+BernCarney@users.noreply.github.com>
2020-04-10 15:01:15 -0600

    added test for version information

--- tests/test_add_a_license.py
+++ tests/test_add_a_license.py
@@ -1,5 +1,11 @@
-from add_a_license import __version__
+from typer.testing import CliRunner
+
+from add_a_license.main import app
+
+runner = CliRunner()
 
 
-def test_version():
-    assert __version__ == "0.1.0"
+def test_add_a_license():
+    result = runner.invoke(app, ["--version"])
+    assert result.exit_code == 0
+    assert "Add A License version: 0.1.0\n" in result.stdout
