e921be8fce49c5804aa11bb95be1b057308bb90e
Bern Carney <18447002+BernCarney@users.noreply.github.com>
2020-04-10 23:08:07 -0600

    added test case for no input, and tests for future functionality

--- tests/test_add_a_license.py
+++ tests/test_add_a_license.py
@@ -5,7 +5,31 @@ from add_a_license.main import app
 runner = CliRunner()
 
 
-def test_add_a_license():
+def test_version():
     result = runner.invoke(app, ["--version"])
     assert result.exit_code == 0
     assert "Add A License version: 0.1.0\n" in result.stdout
+
+
+def test_no_input_help():
+    result = runner.invoke(app)
+    assert result.exit_code == 0
+    assert "Usage: main [OPTIONS] COMMAND [ARGS]...\n\n" in result.stdout
+
+
+def test_list_licenses():
+    result = runner.invoke(app, ["get", "MIT"])
+    assert result.exit_code == 0
+    pass
+
+
+def test_get_license():
+    pass
+
+
+def test_invalid_option():
+    pass
+
+
+def test_invalid_command():
+    pass
